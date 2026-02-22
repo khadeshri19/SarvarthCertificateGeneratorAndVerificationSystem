5a639c251a7fd7392d521362034743e893feea5b
Shrikant Khade <159467028+khadeshri19@users.noreply.github.com>
2026-02-23 00:03:45 +0530

    Removed redundant certificateProject.backend folder

--- Project/certificateProject.backend/src/config/jwt.config.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const JWT_SECRET = process.env.JWT_SECRET || 'secret';
-export const JWT_EXPIRES_IN = '24h';
--- Project/certificateProject.backend/src/middlewares/auth.middlewares.ts
+++
@@ -1,8 +0,0 @@
-import pool from '../script/pool.script';
-
-export const getAllUsers = async ()=>{
-        const result = await pool.query(
-        'SELECT id, name, email, role, created_at FROM users ORDER BY created_at DESC'
-    );
-    return result.rows;
-}
